598165c5d0b4189308926ccbb3eef262a26d65df
marco796 <marcoberlato.music@gmail.com>
2024-06-08 11:38:33 +0200

    ho creato i bottoni delle cifre, i bottoni delle 4 operazioni e le relative funzioni

--- calculator.js
+++ calculator.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const add = function(a, b) {
+    return a + b;
+};
+
+const substract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+console.log(add(7, 3));
+console.log(substract(7, 3));
+console.log(multiply(7, 3));
+console.log(divide(7, 3));
