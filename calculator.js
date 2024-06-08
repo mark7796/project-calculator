437e3fce5cea2f3e1c0bf62da2fded5f70aed881
marco796 <marcoberlato.music@gmail.com>
2024-06-08 14:34:07 +0200

    sono riuscito a far funzionare il botto dello zero

--- calculator.js
+++ calculator.js
@@ -1,3 +1,59 @@
+const whatISeeInDisplay = document.querySelector('#result-display');
+let displayValue = '';
+whatISeeInDisplay.textContent = displayValue;
+
+const updateDisplayValue = function (x) {
+    displayValue = x;
+    whatISeeInDisplay.textContent += displayValue;
+};
+
+
+
+const zeroBtn = document.querySelector('#zero');
+const oneBtn = document.querySelector('#one');
+const twoBtn = document.querySelector('#two');
+const threeBtn = document.querySelector('#three');
+const fourBtn = document.querySelector('#four');
+const fiveBtn = document.querySelector('#five');
+const sixBtn = document.querySelector('#six');
+const sevenBtn = document.querySelector('#seven');
+const eightBtn = document.querySelector('#eight');
+const nineBtn = document.querySelector('#nine');
+
+
+zeroBtn.addEventListener('click', () => {
+    updateDisplayValue(0);
+    });
+oneBtn.addEventListener('click', () => {
+    result += '1';
+});
+twoBtn.addEventListener('click', () => {
+    result += '2';
+});
+threeBtn.addEventListener('click', () => {
+    result += '3';
+});
+fourBtn.addEventListener('click', () => {
+    result += '4';
+});
+fiveBtn.addEventListener('click', () => {
+    result += '5';
+});
+sixBtn.addEventListener('click', () => {
+    result += '6';
+});
+sevenBtn.addEventListener('click', () => {
+    result += '7';
+});
+eightBtn.addEventListener('click', () => {
+    result += '8';
+});
+nineBtn.addEventListener('click', () => {
+    result += '9';
+});
+
+
+
 let firstNumber = 0;
 let operator = '';
 let secondNumber = 0;
